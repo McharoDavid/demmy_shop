7a0ab9e07e91c4d48bc6ce971a8ff60379048858
David Mcharo <mcharodavid@gmail.com>
2020-04-03 17:08:09 -0500

    First Flutter Mobile Application.. completed UI for login and working on the progressDialog.

--- lib/userScreens/loginLogout.dart
+++ lib/userScreens/loginLogout.dart
@@ -1,3 +1,4 @@
+import 'package:demmyshop/tools/app_tools.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -6,18 +7,74 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController email = new TextEditingController();
+  TextEditingController password = new TextEditingController();
+  final scaffoldKey = new GlobalKey<ScaffoldState>();
+  BuildContext context;
+
   @override
   Widget build(BuildContext context) {
+    this.context = context;
     return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Theme.of(context).primaryColor,
       appBar: new AppBar(
+        elevation: 0.0,
         title: new Text("Login"),
         centerTitle: false,
       ),
-      body: new Center(
-        child: new Text("Login",
-          style: new TextStyle(fontSize: 25.0),),
+      body: new SingleChildScrollView(
+        child: new Column(
+          children: <Widget>[
+            new SizedBox(height: 30.0,),
+            appTextField(
+              isPassword: false,
+              sidePadding: 18.0,
+              textHint: "Email Address",
+              textIcon: Icons.email,
+              controller: email
+            ),
+            new SizedBox(height: 30.0,),
+            appTextField(
+                isPassword: true,
+                sidePadding: 18.0,
+                textHint: "Password",
+                textIcon: Icons.lock,
+              controller: password
+            ),
+            new SizedBox(height: 25.0,),
+            appButton(
+              btnTxt: "Login",
+              onBtnClicked: verifyLogin,
+              btnPadding: 20.0,
+              btnColor: Theme.of(context).primaryColor
+            ),
+            new GestureDetector(
+              onTap: (){
+
+              },
+              child: new Text(
+                "Not registered? Sign Up here",
+                style: new TextStyle(color: Colors.white ),
+              ),
+            )
+          ],
+        ),
       ),
     );
+  }
 
+  verifyLogin(){
+    if(email.text == ""){
+      showSnackBar("Email cannot be empty", scaffoldKey);
+      return;
+    }
+
+    if(password.text == ""){
+      showSnackBar("Password cannot be empty", scaffoldKey);
+      return;
+    }
+    
+    displayProgressDialog(context);
   }
 }
