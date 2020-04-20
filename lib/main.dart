ca9023121976bd9603466f8428db5f362a6d4724
David Mcharo <mcharodavid@gmail.com>
2020-04-20 16:30:22 -0500

    First Flutter Mobile Application.. completed making Splash Screen which has company logo(foreign snacks).

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,17 @@
 //import 'package:flutter/material.dart';
 //import 'userScreens/myHomePage.dart';
+import 'dart:async';
 import 'dart:io';
 
 import 'package:demmyshop/userScreens/myHomePage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
 void main() {
-  //HttpOverrides.global = _HttpOverrides();
   runApp(MyApp());
 }
 
-//class _HttpOverrides extends HttpOverrides {
-//  @override
-//  HttpClient createHttpClient(SecurityContext context) {
-//    final HttpClient client = super.createHttpClient(context);
-//    client.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
-//    return client;
-//  }
-//}
 /*
 To get sign in certificate for connecting to firebase:
 keytool -list -v -keystore ~/.android/debug.keystore -alias androiddebugkey -storepass android -keypass android
@@ -35,8 +28,88 @@ class MyApp extends StatelessWidget {
 
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: SplashScreen(), //MyHomePage()
     );
   }
 }
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 7), ()=>     Navigator.of(context).
+    push(new MaterialPageRoute(builder: (BuildContext context) => new MyHomePage())));
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          new Container(
+            decoration: BoxDecoration(color: Colors.white70),
+          ),
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              new Expanded(
+                flex: 2,
+                child: new Container(
+                  child: new Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Image.asset('assets/images/ForeignSnacksLogo.png'),
+                    ],
+                  ),
+                )
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircularProgressIndicator(),
+                    new SizedBox(
+                      height: 30.0,
+                    ),
+                    CircleAvatar(
+                      backgroundColor: Colors.red,
+                      radius: 20.0,
+                      child: Icon(
+                        Icons.shopping_cart,
+                        color: Colors.blue,
+                        size: 20.0,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                      child: Text(
+                        "Online Mobile Store \n For Buying Snacks",
+                        style: TextStyle(color: Colors.black, fontSize: 18.0, fontWeight: FontWeight.bold),
+                      )
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
 
